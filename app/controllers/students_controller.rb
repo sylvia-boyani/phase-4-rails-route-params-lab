c5ef7d81f410118b96bd54c786a7aaf88b4ef5ec
sylvia boyani <sylvia.boyani@student.moringaschool.com>
2022-09-30 13:14:26 +0300

    commit tree 73341fca7f996c042e6bbb9baf3a5ed25324722b

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,23 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+
+    students = if first_name
+      Student.find_by(first_name: first_name)
+    elsif last_name
+      Student.find_by(last_name: last_name)
+    else
+      Student.all
+    end
     render json: students
+  end
+
+    def show
+      student = Student.find(params[:id])
+      render json: student
   end
 
 end
